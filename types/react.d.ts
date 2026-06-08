e74bed1ee37dad1d9ec73382dbb56c46368fb962
Harleen Kaur <harleen@aganitha.ai>
2026-06-08 12:37:21 +0530

    Updat react supported typing

--- types/react.d.ts
+++ types/react.d.ts
@@ -1,9 +1,13 @@
 import type { DuckGridAttributes } from "./index";
+import * as React from "react";
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "duck-grid": DuckGridAttributes;
+      "duck-grid": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & DuckGridAttributes,
+        HTMLElement
+      >;
     }
   }
 }
